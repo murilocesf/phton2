c44e6c1a94f7eb36f8121cfec21823430ee7dd5b
murilocesf <162130133+murilocesf@users.noreply.github.com>
2024-05-17 11:12:02 -0300

    subir arquivo

---
+++ app.py
@@ -0,0 +1,34 @@
+class Animal:
+    def__init__(self,nome,idade,tipo):
+        self.nome = nome
+        self.idade = idade
+        self.tipo = tipo
+
+        def__str__(self):
+            return f"animal : {self.nome}, Idade : {self.idade}, Tipo: {self tipo}"
+
+            class colheita:
+                def__str__(self , tipo, quantidade, data__colheita):
+                    self.tipo = Tipo
+                    self.quantidade = quantidade
+                    self.data_colheita = data__colheita
+
+                    def__str__(self):
+                        return f"Colheita: {self.tipo}, Quantidade: {self.quantidade}, data colheita{self.data_colheita}"
+
+                        class fazenda:
+                            def__init__(self, nome):
+                                self.nome = nome
+                                self.animais = []
+                                self.colheitas = []
+
+                                def adicinoar_animal(self, animal):
+                                    self.animais.append(colheita)
+
+                                    def listar_animais(self):
+                                        for animal in self.animais:
+                                            print(animais)
+
+                                            def listar_colheitas(self):
+                                                for colheitas in self.colheitas:
+                                                    print (coleitas)    
